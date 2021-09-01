b7261bd883b378bf2e13750b5d4140dbbd1512ae
Mandoremee <gpffna777@naver.com>
2021-09-01 15:27:01 +0900

    feat : show multiple keywords

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -44,7 +44,13 @@
           <div class="pt-16">
             <h2 class="text-h4 font-weight-bold pb-4">Today's hot issue</h2>
             <v-row>
-              <v-col cols="10" md="6" lg="4" v-for="i in 6" :key="i">
+              <v-col
+                cols="10"
+                md="6"
+                lg="4"
+                v-for="item in items"
+                :key="item.keyword"
+              >
                 <v-hover
                   v-slot:default="{ hover }"
                   open-delay="50"
