bf8d9c402301b9e10b74a28c22eb330099de2f04
Mandoremee <gpffna777@naver.com>
2021-09-03 15:48:47 +0900

    feat : chartjs test but failed(commented)

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,3 +1,82 @@
+<!--<script>
+export default {
+  data: () => ({
+    skill: 20,
+    knowledge: 33,
+    power: 78,
+  }),
+};
+</script> -->
+
+<!--  chartjs test but it doesn't work!!
+<script>
+import Chart from "chart.js";
+var ctx = document.getElementById("myChart").getContext("2d");
+var myChart = new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+myChart();
+
+/* eslint-disable no-unused-vars */
+// import Chart from "chart.js";
+// import planetChartData from "./chart-data.js";
+// export default {
+//   data() {
+//     return {
+//       planetChartData: planetChartData,
+//     };
+//   },
+//   method: {
+//     createChart(chartId, chartData) {
+//       const ctx = document.getElementById(chartId);
+//       const myChart = new Chart(ctx, {
+//         type: chartData.type,
+//         data: chartData.data,
+//         options: chartData.options,
+//       });
+//     },
+//   },
+//   mounted() {
+//     this.createChart("planet-chart", this.planetChartData);
+//   },
+// };
+/* eslint-disable no-unused-vars */
+</script>
+-->
+
 <template>
   <div>
     <v-row>
