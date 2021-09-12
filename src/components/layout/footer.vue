b680323e944cac3ca5c492aa518f36a9fad0d15e
Mandoremee <gpffna777@naver.com>
2021-09-12 19:18:35 +0900

    feat & style : footer contents change

--- src/components/layout/footer.vue
+++ src/components/layout/footer.vue
@@ -3,19 +3,18 @@
     <v-footer color="primary" dark app absolute>
       <v-container>
         <v-row align="start">
-          <v-col cols="12" md="3">
-            <h4 class="text-h5 font-weight-bold">
-              Giraffe
-            </h4>
+          <v-col cols="10" md="4">
+            <h4 class="text-h6 font-weight-bold">3S1C 댓글 분석기</h4>
             <p class="text-body-1">
-              Create, prototype, collaborate and turn your ideas into incredible
-              products with the definitive platform for digital design.
+              <br />
+              2021년도 동국대학교 컴퓨터공학 종합설계 "빅데이터 기반 댓글 분석
+              및 시각화" 프로젝트입니다.
             </p>
           </v-col>
 
-          <v-col cols="12" md="3">
+          <v-col cols="10" md="4">
             <v-list flat color="transparent">
-              <v-subheader class="text-h6">HeroUI</v-subheader>
+              <v-subheader class="text-h6">Project Sources</v-subheader>
               <v-list-item-group color="primary">
                 <v-list-item
                   v-for="(item, i) in heroui"
@@ -35,9 +34,9 @@
             </v-list>
           </v-col>
 
-          <v-col cols="12" md="3">
+          <v-col cols="10" md="4">
             <v-list flat color="transparent">
-              <v-subheader class="text-h6">Giraffe</v-subheader>
+              <v-subheader class="text-h6">UI reference</v-subheader>
               <v-list-item-group color="primary">
                 <v-list-item
                   v-for="(item, i) in shock"
@@ -57,11 +56,9 @@
             </v-list>
           </v-col>
 
-          <v-col cols="12" md="3">
+          <!-- <v-col cols="12" md="3">
             <div>
-              <h5 class="text-h6 font-weight-bold">
-                Subscribe
-              </h5>
+              <h5 class="text-h6 font-weight-bold">Subscribe</h5>
               <p class="text-body-1">
                 The latest Rocket news, articles, and resources, sent straight
                 to your inbox every month.
@@ -79,22 +76,20 @@
               </v-col>
 
               <v-col cols="4">
-                <v-btn large block color="secondary">
-                  Subscribe
-                </v-btn>
+                <v-btn large block color="secondary"> Subscribe </v-btn>
               </v-col>
             </v-row>
-          </v-col>
+          </v-col> -->
         </v-row>
 
         <div class="text-center mt-6">
           Copyright &copy;
           <a
             class="white--text"
-            href="https://heroui.net"
+            href="https://github.com/CSID-DGU"
             target="_blank"
             rel="noopener noreferrer"
-            >HreoUI</a
+            >CSID-DGU</a
           >
           {{ new Date().getFullYear() }}. All rights reserved.
         </div>
@@ -109,11 +104,11 @@ export default {
     heroui: [
       {
         text: "Github",
-        href: "https://github.com/AGDholo",
+        href: "https://github.com/CSID-DGU/2021-1-CECD4-3S1C-2",
       },
       {
         text: "Themes",
-        href: "https://heroui.net/",
+        href: "https://github.com/CSID-DGU/3S1C_frontend",
       },
     ],
     shock: [
