a9839ab4ffaf4eb3a1e90f833da1541efdde6d5e
Mandoremee <gpffna777@naver.com>
2021-11-02 15:41:26 +0900

    style : temp radar chart data, style change

--- src/components/details/radar.vue
+++ src/components/details/radar.vue
@@ -1,4 +1,4 @@
-a<script>
+<script>
 import { Radar } from "vue-chartjs";
 export default {
   extends: Radar,
@@ -19,7 +19,7 @@ export default {
         datasets: [
           {
             label: "My First Dataset",
-            data: [65, 59, 90, 81],
+            data: [47, 8, 11602, 14],
             fill: true,
             backgroundColor: "rgba(255, 99, 132, 0.2)",
             borderColor: "rgb(255, 99, 132)",
@@ -30,6 +30,34 @@ export default {
           },
         ],
       },
+
+      options: {
+        legend: {
+          display: false,
+        },
+        elements: {
+          line: {
+            backgroundColor: "rgb(255, 99, 132)",
+            borderColor: "rgb(255, 99, 132)",
+            borderWidth: 5,
+          },
+          point: {
+            backgroundColor: "rgb(255, 99, 132)",
+            hoverBackgroundColor: "rgb(255, 99, 132)",
+            radius: 5,
+            hoverRadius: 10,
+          },
+        },
+      },
+      // scales: {
+      //   r: {
+      //     max: 10000,
+      //     min: 0,
+      //     ticks: {
+      //       stepSize: 2000,
+      //     },
+      //   },
+      // },
     };
   },
   mounted() {
